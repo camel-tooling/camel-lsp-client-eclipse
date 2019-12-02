ef99c3982bde2a55757a7090094fca057d45ce47
Aurélien Pupier <apupier@redhat.com>
2019-12-02 13:52:38 +0100

    FUSETOOLS2-72 - Preference to add additional components
    - needs to be provided as a List of components respecting the JSON Camel
    Catalog syntax
    - no validation in UI, if invalid, just ignored it with a trace in log.
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- com.github.camel-tooling.lsp.eclipse.client/src/com/github/cameltooling/eclipse/preferences/CamelLanguageServerPreferenceManager.java
+++ com.github.camel-tooling.lsp.eclipse.client/src/com/github/cameltooling/eclipse/preferences/CamelLanguageServerPreferenceManager.java
@@ -16,37 +16,65 @@
  */
 package com.github.cameltooling.eclipse.preferences;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.osgi.service.prefs.BackingStoreException;
 
 import com.github.cameltooling.eclipse.client.ActivatorCamelLspClient;
+import com.google.gson.Gson;
 
 public class CamelLanguageServerPreferenceManager {
 
 	private static final String TOP_NODE_CAMEL_KEY = "camel";
 	static final String CAMEL_CATALOG_VERSION_PREF_KEY = "Camel catalog version";
+	static final String CAMEL_ADDITIONAL_COMPONENT_PREF_KEY = "extra-components";
 
-	public Map<String, Map<String, String>> getPreferenceAsLanguageServerFormat() {
+	public Map<String, Map<String, Object>> getPreferenceAsLanguageServerFormat() {
 		IEclipsePreferences preferences = getCamelPreferenceNode();
-		Map<String, Map<String, String>> settings = new HashMap<>();
-		Map<String, String> camelSettings = new HashMap<>();
+		Map<String, Map<String, Object>> settings = new HashMap<>();
+		Map<String, Object> camelSettings = new HashMap<>();
 		camelSettings.put(CAMEL_CATALOG_VERSION_PREF_KEY, preferences.get(CAMEL_CATALOG_VERSION_PREF_KEY, null));
+		camelSettings.put(CAMEL_ADDITIONAL_COMPONENT_PREF_KEY, getAdditionalComponentIfValid(preferences));
 		settings.put(TOP_NODE_CAMEL_KEY, camelSettings );
 		return settings;
 	}
+
+	private List<?> getAdditionalComponentIfValid(IEclipsePreferences preferences) {
+		String additionalComponentAsString = preferences.get(CAMEL_ADDITIONAL_COMPONENT_PREF_KEY, null);
+		if(additionalComponentAsString != null && !additionalComponentAsString.isEmpty()) {
+			try {
+				return new Gson().fromJson(additionalComponentAsString, List.class);
+			} catch (Exception ex) {
+				ActivatorCamelLspClient.getInstance().getLog().log(new Status(IStatus.ERROR, ActivatorCamelLspClient.ID, "Invalid JSON provided for extra Camel components.", ex));
+				return Collections.emptyList();
+			}
+		}
+		return Collections.emptyList();
+	}
 	
 	public void setCamelCatalogVersion(String camelVersion) throws BackingStoreException {
+		setPreferenceValue(camelVersion, CAMEL_CATALOG_VERSION_PREF_KEY);
+	}
+
+	private void setPreferenceValue(String camelVersion, String key) throws BackingStoreException {
 		IEclipsePreferences preferences = getCamelPreferenceNode();
 		if (camelVersion == null || camelVersion.isEmpty()) {
-			preferences.remove(CAMEL_CATALOG_VERSION_PREF_KEY);
+			preferences.remove(key);
 		} else {
-			preferences.put(CAMEL_CATALOG_VERSION_PREF_KEY, camelVersion);
+			preferences.put(key, camelVersion);
 		}
 		preferences.flush();
+	}
+	
+	public void setCamelAdditionalComponents(String additionalComponents) throws BackingStoreException {
+		setPreferenceValue(additionalComponents, CAMEL_ADDITIONAL_COMPONENT_PREF_KEY);
 	}
 
 	private IEclipsePreferences getCamelPreferenceNode() {
