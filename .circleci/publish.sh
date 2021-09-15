023f407477f9405244ee6af00455abe8aac3da0e
Aurélien Pupier <apupier@redhat.com>
2021-09-15 15:48:08 +0200

    FUSETOOLS2-1298 - migrate default branch name from master to main
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -12,7 +12,7 @@ SHA=`git rev-parse --verify HEAD`
 cd $HOME
 git config --global user.email "circleci@circle-ci.org"
 git config --global user.name "circle-ci"
-git clone --quiet --branch=master https://${GH_PAGES_DEPLOY}@github.com/camel-tooling/camel-lsp-client-eclipse-update-site.git gh-pages > /dev/null
+git clone --quiet --branch=main https://${GH_PAGES_DEPLOY}@github.com/camel-tooling/camel-lsp-client-eclipse-update-site.git gh-pages > /dev/null
 cd gh-pages
 
 # delete existing data
@@ -23,6 +23,6 @@ cp -r $REPO_PATH/com.github.camel-tooling.lsp.eclipse.updatesite/target/updatesi
 git add -f .
 git status
 git commit -m "Deploy ${DEPLOY_REPO_ROOT} update site to GitHub Pages: ${CIRCLE_SHA1} from Circle CI build $CIRCLE_BUILD_NUM"
-git push -fq origin master > /dev/null
+git push -fq origin main > /dev/null
 # return to original path
 cd $REPO_PATH
